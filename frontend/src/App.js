ec0e85df6417eea5a54ec9ff8588352fa38efaff
melvin555 <melvin_harsono@yahoo.com>
2021-10-06 13:11:27 +0800

    OctoberUpdate

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+
+class App extends Component{
+  constructor(props){
+    super(prop);
+    this.state = {
+      viewCompleted:false;
+      taskList:
+    }
+  }
+}
+
+export default App;
